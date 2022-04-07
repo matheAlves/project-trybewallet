da727d69d6421295dbcaa13b8793999ac2bd659b
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-04-07 00:05:36 -0300

    requisitos 7 & 8 :heavy_check_mark:

---
+++ src/components/ExpenseTable.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+class ExpenseTable extends React.Component {
+  render() {
+    const { expenses } = this.props;
+    return (
+      <table>
+        <thead>
+          <tr>
+            <th>Descrição</th>
+            <th>Tag</th>
+            <th>Método de pagamento</th>
+            <th>Valor</th>
+            <th>Moeda</th>
+            <th>Câmbio utilizado</th>
+            <th>Valor convertido</th>
+            <th>Moeda de conversão</th>
+            <th>Editar/Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense) => (
+            <tr key={ expense.id }>
+              <td>
+                {expense.description}
+              </td>
+              <td>
+                {expense.tag}
+              </td>
+              <td>
+                {expense.method}
+              </td>
+              <td>
+                {Number.parseFloat(expense.value).toFixed(2)}
+              </td>
+              <td>
+                {expense.exchangeRates[expense.currency].name}
+              </td>
+              <td>
+                {Number.parseFloat(
+                  expense.exchangeRates[expense.currency].ask,
+                ).toFixed(2)}
+              </td>
+              <td>
+                {Number.parseFloat(
+                  expense.value * expense.exchangeRates[expense.currency].ask,
+                ).toFixed(2)}
+              </td>
+              <td>
+                <p>Real</p>
+              </td>
+              <td>
+                <button
+                  type="button"
+                >
+                  Excluir
+                </button>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    );
+  }
+}
+
+ExpenseTable.propTypes = {
+  expenses: PropTypes.arrayOf(PropTypes.object).isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  expenses: state.wallet.expenses,
+});
+
+export default connect(mapStateToProps)(ExpenseTable);
