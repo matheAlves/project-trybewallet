f457fa414664db794fb47384f9bf08fadea570bb
Matheus S Alves <matheus.souza.alves@gmail.com>
2022-09-04 15:59:03 -0300

    Updates

---
+++ precache-manifest.e6b2839f8c0c9ae82143e9217b209ba1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2799fe2d92e4a7ef64cea4fd27052497",
+    "url": "/project-trybewallet/index.html"
+  },
+  {
+    "revision": "0c0f0206ef5fd77d18ad",
+    "url": "/project-trybewallet/static/css/main.21548203.chunk.css"
+  },
+  {
+    "revision": "135b1f70df0288f71cc8",
+    "url": "/project-trybewallet/static/js/2.0ce20643.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/project-trybewallet/static/js/2.0ce20643.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c0f0206ef5fd77d18ad",
+    "url": "/project-trybewallet/static/js/main.84a8171d.chunk.js"
+  },
+  {
+    "revision": "f7e9fac6e1746606b827",
+    "url": "/project-trybewallet/static/js/runtime-main.29301b93.js"
+  }
+]);
